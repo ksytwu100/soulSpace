2b1fcf0c95b9a5dc8bfa9b489e93b875289c1d7d
Arman Tork <canadienperse@gmail.com>
2025-01-27 23:12:31 -0500

    added conditional rating

--- client/src/components/Rating/index.jsx
+++ client/src/components/Rating/index.jsx
@@ -1,13 +1,32 @@
-import Rating from '@mui/material/Rating';
+import { useState } from "react";
+import Rating from "@mui/material/Rating";
 import { Box } from "@mui/material";
+import Auth from "../../utils/auth";
 
 const CarRating = (props) => {
-    return (
+  // State to manage the controlled rating if the user is logged in
+  const [rating, setRating] = useState(props.value);
 
-        <Box sx={{ '& > legend': { mt: 2 } }}>
-            <Rating name="read-only" value={props.value} precision={0.1} readOnly />
-        </Box>
-    );
-}
+  // Check if the user is logged in
+  const isLoggedIn = Auth.loggedIn();
+
+  const handleRatingChange = (event, newValue) => {
+    if (isLoggedIn) {
+      setRating(newValue);
+    }
+  };
+
+  return (
+    <Box sx={{ "& > legend": { mt: 2 } }}>
+      <Rating
+        name={isLoggedIn ? "uncontrolled" : "read-only"}
+        value={isLoggedIn ? rating : props.value}
+        precision={0.1}
+        onChange={handleRatingChange}
+        readOnly={!isLoggedIn}
+      />
+    </Box>
+  );
+};
 
 export default CarRating;
